209b4bce599ef0b88686b79cdbfc7e2680a2859d
zcong1993 <1005997919@qq.com>
2017-06-28 15:17:24 +0800

    backup

--- db/create.js
+++ db/create.js
@@ -1,7 +1,7 @@
 const db = require('../src/db')
 
-function up() {
-  return db.schema.createTable('users', t => {
+async function up() {
+  await db.schema.createTable('users', t => {
     t.increments('id').primary()
     t.string('username', 100).notNullable()
     t.string('password', 100).notNullable()
@@ -11,31 +11,27 @@ function up() {
     t.integer('status').defaultTo(1)
     t.unique('username')
   })
-    .then(() => {
-      return db.schema.createTable('posts', t => {
-        t.increments('id').primary()
-        t.string('title', 200).notNullable()
-        t.text('description')
-        t.text('content')
-        t.integer('author_id').notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE')
-        t.integer('status').defaultTo(1)
-        t.timestamps()
-        t.unique('title')
-      })
-    })
-    .then(() => {
-      return db.schema.createTable('videos', t => {
-        t.increments('id').primary()
-        t.string('title', 200).notNullable()
-        t.text('description')
-        t.string('link').notNullable()
-        t.integer('author_id').notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE')
-        t.integer('status').defaultTo(1)
-        t.timestamps()
-        t.unique('title')
-      })
-    })
-    .then(() => console.log('Done: up!'))
+  await db.schema.createTable('posts', t => {
+    t.increments('id').primary()
+    t.string('title', 200).notNullable()
+    t.text('description')
+    t.text('content')
+    t.integer('author_id').notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE')
+    t.integer('status').defaultTo(1)
+    t.timestamps()
+    t.unique('title')
+  })
+  await db.schema.createTable('videos', t => {
+    t.increments('id').primary()
+    t.string('title', 200).notNullable()
+    t.text('description')
+    t.string('link').notNullable()
+    t.integer('author_id').notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE')
+    t.integer('status').defaultTo(1)
+    t.timestamps()
+    t.unique('title')
+  })
+  console.log('Done: up!')
 }
 
 async function down() {
