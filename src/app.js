937137cceb3578f2d3797cca53972f72b91990bf
zcong1993 <1005997919@qq.com>
2017-07-17 14:57:18 +0800

    support application/graphql

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const logger = require('koa-logger')
 const bodyParser = require('koa-bodyparser')
 const cors = require('kcors')
 const Router = require('koa-router')
+const static = require('koa-static')
 const { graphqlKoa, graphiqlKoa } = require('graphql-server-koa')
+const { apolloUploadKoa } = require('apollo-upload-server')
 const schema = require('./schema')
 const { user, post, video, img, userPosts, userVideos, userImgs } = require('./dataloader')
 
@@ -17,19 +19,52 @@ onerror(app)
 if (process.env.NODE_ENV !== 'test') {
   app.use(logger())
 }
-app.use(bodyParser())
-router.post('/graphql', graphqlKoa({
-  schema,
-  context: {
-    user,
-    post,
-    video,
-    img,
-    userPosts,
-    userVideos,
-    userImgs
-  }
-}))
+
+app.use(static('static'))
+
+// app.use(bodyParser({
+//   enableTypes: ['json', 'text', 'form'],
+//   extendTypes: {
+//     text: ['application/graphql']
+//   }
+// }))
+
+const mws = [
+  bodyParser({
+    enableTypes: ['json', 'text', 'form'],
+    extendTypes: {
+      text: ['application/graphql'],
+      json: ['application/json']
+    }
+  }),
+  async (ctx, next) => {
+    if (ctx.request.is('application/graphql')) {
+      ctx.request.body = { query: ctx.request.body }
+    }
+    console.log(ctx.request.body)
+    await next()
+  },
+  apolloUploadKoa({
+    uploadDir: './static/imgs'
+  })
+]
+
+router.post(
+  '/graphql',
+  ...mws,
+  graphqlKoa({
+    schema,
+    context: {
+      user,
+      post,
+      video,
+      img,
+      userPosts,
+      userVideos,
+      userImgs
+    }
+  })
+)
 
 if (ENV !== 'prod') {
   router.get('/graphql', graphiqlKoa({ endpointURL: '/graphql' }))
